a565d2df37372759cca076e0848f0c774376ee76
Wessel van den IJssel <wessel@mbbma.nl>
2023-12-07 10:04:12 +0100

    init

---
+++ Practicum 6b/src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Game> teKoop = new ArrayList<Game>();
+        teKoop.add(new Game("Mario Kart 8", 2019, 35.00));
+        teKoop.add(g1);
+        ArrayList<Game> nogNietInBezit = p1.bepaalGamesNietInBezit(teKoop);
+        System.out.println("p1 heeft de volgende games nog niet: " + nogNietInBezit.toString());
+    }
+}
